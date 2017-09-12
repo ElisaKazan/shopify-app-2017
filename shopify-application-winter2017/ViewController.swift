d6fd76b7eafae98084c4640ee724390f8c14c6e4
Elisa Kazan <elisa.kazan@gmail.com>
2017-09-11 21:47:59 -0400

    Initial Commit

---
+++ shopify-application-winter2017/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shopify-application-winter2017
+//
+//  Created by Elisa Kazan on 2017-09-11.
+//  Copyright © 2017 ElisaKazan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ shopify-application-winter2017UITests/shopify_application_winter2017UITests.swift
@@ -0,0 +1,36 @@
+//
+//  shopify_application_winter2017UITests.swift
+//  shopify-application-winter2017UITests
+//
+//  Created by Elisa Kazan on 2017-09-11.
+//  Copyright © 2017 ElisaKazan. All rights reserved.
+//
+
+import XCTest
+
+class shopify_application_winter2017UITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
